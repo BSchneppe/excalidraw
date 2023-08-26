f75cc2f58a9184157f30aeb2432b80e0617b2a38
Benedikt Schneppe <benedikt@flowers-software.de>
2023-08-26 09:54:03 +0200

    change autorelease.js

--- scripts/autorelease.js
+++ scripts/autorelease.js
@@ -1,23 +1,25 @@
 const fs = require("fs");
-const { exec, execSync } = require("child_process");
+const {execSync } = require("child_process");
 const core = require("@actions/core");
 
 const excalidrawDir = `${__dirname}/../src/packages/excalidraw`;
 const excalidrawPackage = `${excalidrawDir}/package.json`;
 const pkg = require(excalidrawPackage);
-const isPreview = process.argv.slice(2)[0] === "preview";
 
-const getShortCommitHash = () => {
-  return execSync("git rev-parse --short HEAD").toString().trim();
-};
 
 const publish = () => {
-  const tag = isPreview ? "preview" : "next";
+  const tag ="preview";
 
   try {
     execSync(`yarn  --frozen-lockfile`);
     execSync(`yarn --frozen-lockfile`, { cwd: excalidrawDir });
-    execSync(`yarn run build:umd`, { cwd: excalidrawDir });
+    try {
+      execSync(`yarn run build:umd`, { cwd: excalidrawDir });
+    } catch (error) {
+      console.error('Error executing `yarn run build:umd`:');
+      console.error(error.stdout && error.stdout.toString());  // Outputs stdout (if any)
+      console.error(error.stderr && error.stderr.toString());  // Outputs the error message
+    }
     execSync(`yarn --cwd ${excalidrawDir} publish --tag ${tag}`);
     console.info(`Published ${pkg.name}@${tag}🎉`);
     core.setOutput(
@@ -31,42 +33,13 @@ const publish = () => {
     process.exit(1);
   }
 };
-// get files changed between prev and head commit
-exec(`git diff --name-only HEAD^ HEAD`, async (error, stdout, stderr) => {
-  if (error || stderr) {
-    console.error(error);
-    core.setOutput("result", ":warning: Package couldn't be published!");
-    process.exit(1);
-  }
-  const changedFiles = stdout.trim().split("\n");
-  const filesToIgnoreRegex = /src\/excalidraw-app|packages\/utils/;
 
-  const excalidrawPackageFiles = changedFiles.filter((file) => {
-    return (
-      (file.indexOf("src") >= 0 || file.indexOf("package.json")) >= 0 &&
-      !filesToIgnoreRegex.test(file)
-    );
-  });
-  if (!excalidrawPackageFiles.length) {
-    console.info("Skipping release as no valid diff found");
-    core.setOutput("result", "Skipping release as no valid diff found");
-    process.exit(0);
-  }
 
-  // update package.json
-  let version = `${pkg.version}-${getShortCommitHash()}`;
+let version = `${pkg.version}-${process.argv.slice(2)[0]}`;
 
-  // update readme
+pkg.version = version;
 
-  if (isPreview) {
-    // use pullNumber-commithash as the version for preview
-    const pullRequestNumber = process.argv.slice(3)[0];
-    version = `${pkg.version}-${pullRequestNumber}-${getShortCommitHash()}`;
-  }
-  pkg.version = version;
+fs.writeFileSync(excalidrawPackage, JSON.stringify(pkg, null, 2), "utf8");
 
-  fs.writeFileSync(excalidrawPackage, JSON.stringify(pkg, null, 2), "utf8");
-
-  console.info("Publish in progress...");
-  publish();
-});
+console.info("Publish in progress...");
+publish();
